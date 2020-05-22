0b79594d5d00220b8ca57e7368f6b6f783211ebe
aidanakad <aydana0504@gmail.com>
2020-05-22 18:40:54 +0600

    added comments in js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 fetch('https://randomuser.me/api/?results=8') 
-    .then(response => response.json())
+    .then(response => response.json()) //получила ответ от бэкэнда
     .then(data => {
-        const team = document.querySelector('#team')
+        const team = document.querySelector('#team') //полусила доступ к html тэгу
 
         data.results.sort((male, female) => {
             return male.gender - female.gender
-        })
+        }) //пыталась сделать сортировку по полу
 
-        data.results.forEach(member => {
-            const box =document.createElement('div')
+        data.results.forEach(member => { //пробежалась по циклу
+            const box =document.createElement('div') //создала дом элемент
             box.className = 'box'
 
             const name = document.createElement('div')
@@ -16,13 +16,13 @@ fetch('https://randomuser.me/api/?results=8')
             const photo = document.createElement('img')
             photo.className='pic'
 
-            photo.setAttribute("src", member.picture.large)
+            photo.setAttribute("src", member.picture.large) //добавила атрибуты и присвоила значения из объекта
             photo.setAttribute("alt", "avatar")
             
-            name.textContent = `${member.name.first} ${member.name.last}`
+            name.textContent = `${member.name.first} ${member.name.last}` //присвоила значения из объекта
             name.className = 'name'
 
-            team.appendChild(box)
+            team.appendChild(box) //вставила в корневой элемент
             box.appendChild(photo)
             box.appendChild(name)
            
