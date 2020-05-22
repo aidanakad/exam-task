d285baee1d4f050b84e20f6d93803cbc03adf4d1
aidanakad <aydana0504@gmail.com>
2020-05-22 16:54:28 +0600

    added style for team block

--- script.js
+++ script.js
@@ -3,21 +3,30 @@ fetch('https://randomuser.me/api/?results=8')
     .then(data => {
         const team = document.querySelector('#team')
 
+        data.results.sort((male, female) => {
+            return male.gender - female.gender
+        })
+
         data.results.forEach(member => {
+            const box =document.createElement('div')
+            box.className = 'box'
 
             const name = document.createElement('div')
 
             const photo = document.createElement('img')
             photo.className='pic'
-            
+
             photo.setAttribute("src", member.picture.large)
             photo.setAttribute("alt", "avatar")
             
             name.textContent = `${member.name.first} ${member.name.last}`
             name.className = 'name'
 
-            team.appendChild(name)
-            team.appendChild(photo)
+            team.appendChild(box)
+            box.appendChild(photo)
+            box.appendChild(name)
+
+           
         })
     })
 
